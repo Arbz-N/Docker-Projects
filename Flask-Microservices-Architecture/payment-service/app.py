c1054a3a488d685bd56ff1c66afcb9a0107fbfcb
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-01-26 06:30:14 +0500

    added payment-service folder

---
+++ Flask-Microservices-Architecture/payment-service/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+
+SERVICE_NAME = "payment-service"
+
+MONGO_URL = os.getenv("MONGO_URL")
+client = MongoClient(MONGO_URL)
+db = client["microservice_logs"]
+logs = db["logs"]
+
+def log_events(level,message):
+    logs.insert_one({
+        "Service":"SERVICE_NAME",
+        "level": level,
+        "message": message,
+        "timestamp": datetime.utcnow(),
+        "host": os.getenv("HOSTNAME", "auth-container")
+    })
+
+@app.route("/order",methods=["POST"])
+def login():
+    log_events("INFO", "Payment initiated")
+    log_events("INFO", "Payment successful")
+    return jsonify({"status":"payment done"})
+
+
